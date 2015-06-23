80a2e885fb14fca6bb558da0f68825931d30f83e
Aaron W <chlortho81@gmail.com>
2015-06-23 13:19:44 -0400

    added operator project to repo

---
+++ MathEx.java
@@ -0,0 +1,34 @@
+// this is Aaron Whittle's Math example
+
+class MathEx {
+
+    public static void main(String args[]){
+
+        int i = 5;
+        int j = 3;
+        long l = 17L; //the L need not be capitalized
+        float f = 3.5f;// note that the "f" need not not be capitalized
+        double d = 42.9;
+
+        i = i + 12; // addition
+
+        j = j - 5; // subtration
+
+        l = j / i; // division
+
+        f = f * d; // error why????? fix it!
+        
+        i = 17 % 3; // modulus
+
+
+        System.out.println(" i is " + i);
+        System.out.println(" l is " + l);
+        System.out.println(" f is " + f);
+        System.out.println(" d is " + d);
+
+    }
+
+}
+
+
+
